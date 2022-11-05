497a2ed1f1f60a7a00eac380fbb62c8d6692b451
airat <ajratbastanov@gmail.com>
2022-11-05 12:12:06 +0300

    Complite

---
+++ Demo/Demo_exam/NewOrderCreateWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Demo_exam
+{
+    /// <summary>
+    /// Логика взаимодействия для NewOrderCreateWindow.xaml
+    /// </summary>
+    public partial class NewOrderCreateWindow : Window
+    {
+        public NewOrderCreateWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
